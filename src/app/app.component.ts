f8139b3d7a92e9fee5fcf6cd9b8a7aafe0b1063d
Yevhenii Melnyk <rpm.steel@gmail.com>
2025-02-22 15:33:07 +0200

    made profile service and render of cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ProfileCardComponent} from './common-ui/profile-card/profile-card.component';
+import {ProfileService} from './data/services/profile.service';
+import {CommonModule} from '@angular/common';
+import {Profile} from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCardComponent],
+  imports: [RouterOutlet, ProfileCardComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
 })
 export class AppComponent {
-  title = 'tik-talk';
+  profileService = inject(ProfileService);
+  profiles: Profile[] = []
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe(accounts => {
+      this.profiles = accounts;
+    })
+  }
 }
--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Profile} from '../../data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-profile-card',
   imports: [],
   templateUrl: './profile-card.component.html',
-  styleUrl: './profile-card.component.scss'
+  styleUrl: './profile-card.component.scss',
+  standalone: true,
 })
 export class ProfileCardComponent {
-
+  @Input() profile!: Profile;
 }
