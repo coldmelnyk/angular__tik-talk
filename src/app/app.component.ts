0de53c2647f3872707d3a3cfb25cb8c8312a7568
Yevhenii Melnyk <rpm.steel@gmail.com>
2025-02-24 16:53:59 +0200

    made basic routes and space for layout of sidebar where needed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,14 @@
-import {Component, inject} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ProfileCardComponent} from './common-ui/profile-card/profile-card.component';
-import {ProfileService} from './data/services/profile.service';
 import {CommonModule} from '@angular/common';
-import {Profile} from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCardComponent, CommonModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
 })
 export class AppComponent {
-  profileService = inject(ProfileService);
-  profiles: Profile[] = []
 
-  constructor() {
-    this.profileService.getTestAccounts().subscribe(accounts => {
-      this.profiles = accounts;
-    })
-  }
 }
